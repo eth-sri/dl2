05e2d3fb4e2b00ef5968bf28d26219750a580908
Marc Fischer <spam@totallyawesome.at>
2023-02-09 22:48:43 +0100

    Update run.sh

--- training/semisupservised/run.sh
+++ training/semisupservised/run.sh
@@ -2,7 +2,7 @@
 
 # train model
 python main.py --lr 0.001 --net_type vgg --constraint none --epochs 1600  --num_labeled 100 --dataset cifar100 --exp_name baseline --constraint-weight 0
-python main.py --lr 0.001 --net_type vgg --constraint none --epochs 1600  --num_labeled 100 --dataset cifar100 --exp_name dl2_06 --constraint-weight 0.6
+python main.py --lr 0.001 --net_type vgg --constraint DL2 --epochs 1600  --num_labeled 100 --dataset cifar100 --exp_name dl2_06 --constraint-weight 0.6
 
 # run evaluation
 python main.py --net_type vgg --dataset cifar100 --resume_from vgg_42_baseline_overall_  --testOnly
